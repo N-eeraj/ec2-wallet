a6a7fb97643ea574a859b1916e65c9050c1cc1c8
N-eeraj <neeraj7rajeena@gmail.com>
2025-06-19 00:18:21 +0530

    initial commit

---
+++ apps/web-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "@/main.css"
+
+function App() {
+  return (
+    <>
+      <h1 className="text-primary">
+        Hi
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ apps/web-app/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@router": path.resolve(__dirname, "./src/router"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+    },
+  },
+})
